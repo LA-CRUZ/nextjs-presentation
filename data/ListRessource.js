622b3744345ef15cdacb01e1cd281e76f840deb9
Mickael Alves <0mickael.alves0@gmail.com>
2020-04-12 22:15:27 +0200

    Change name player

--- data/ListRessource.js
+++ data/ListRessource.js
@@ -18,12 +18,12 @@ var list = new ListResources()
 
 list.push(new GeoRessources("Mickael"))
 list.push(new GeoRessources("Lucas"))
-list.push(new GeoRessources("Étienne"))
+list.push(new GeoRessources("Etienne"))
 list.push(new GeoRessources("Pierre"))
 
 list.get("Lucas").addTrophy("React and Vue")
 list.get("Mickael").addTrophy("SCSS Master")
-list.get("Étienne").addTrophy("Git commit Sepuku")
+list.get("Etienne").addTrophy("Git commit Sepuku")
 list.get("Pierre").addTrophy("Wanabe web developer")
 
 module.exports = list;
